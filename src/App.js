a2dd2ddef18b03f14a457885d97d962199d5664c
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-10-10 00:35:50 -0700

    organized code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import Hero from "./components/hero"
+import {Hero } from "./components"
+// import Hero from "./components/hero"
 
 import "./assets/scss/base.scss"
 
 function App() {
   return (
     <div className="App">
+      <h1>Hi!</h1>
       <Hero />
     </div>
   );
