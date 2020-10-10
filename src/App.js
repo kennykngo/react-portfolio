b0de52ed59d528b640dabcc5d37f1496fbb8eb95
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-10-10 12:50:57 -0700

    adding pages, styling navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Redirect, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
+import PageRenderer from "./page-renderer"
 import { Hero, Navigation } from "./components"
 
 import "./assets/scss/base.scss"
@@ -17,7 +18,9 @@ function App() {
       <div className="App">
         <Navigation />
         <Switch>
-
+          <Route path="/page" component={PageRenderer} />
+          <Route path="/" render={()=> <Redirect to="/home" />} />
+          <Route component={() => 404} />
         </Switch>
         <Hero />
     
