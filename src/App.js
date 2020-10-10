a3d8401832900e98aa06aafda49dd211ba63df3e
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-10-10 00:14:41 -0700

    added hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import './App.css';
+import Hero from "./components/hero/hero"
 
 function App() {
   return (
     <div className="App">
-
+      <Hero />
     </div>
   );
 }
---
+++ src/components/hero/hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Col, Row } from "react-flexbox-grid";
+
+const Hero = () => {
+    return (
+    <div className="hero">
+        <div className="wrapper">
+            <Row> 
+                <Col>Hi</Col>
+            </Row>
+        </div>
+    </div>
+    )
+}
+
+export default Hero;
