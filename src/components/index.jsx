70830d6120af0cac401b8b6242d4fc8f3906821f
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-10-13 00:28:50 -0700

    removed pages directory

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,9 @@
 import Hero from "./hero.jsx";
 import Navigation from "./navigation.jsx";
+import ToggleTheme from "./toggle-theme"
 
 export { 
     Hero,
-    Navigation
+    Navigation,
+    ToggleTheme,
 };
