2c166d055797c00cc6ab8ba2ffb0b1df1e1947a7
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-10-16 18:05:59 -0700

    added _experience-comp.scss

--- src/components/ui-components/experience-comp.js
+++ src/components/ui-components/experience-comp.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {ThemeContext} from "../theme-provider";
 
 export default function ExperienceComp (props) { 
 
+	const state = useContext(ThemeContext);
+
+	const headingStyle = {
+		color: state.theme.text,
+		backgroundColor: state.theme.primary,
+	}
+
 	return(
 	<div className="experience-box">
-		<h2>{props.title}</h2>
+		<h2 style={headingStyle}>{props.title}</h2>
 	</div>	
 	)
 }
