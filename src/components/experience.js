e6feba30a1124da53799972da527e46c222d6271
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-10-18 01:57:32 -0700

    converting class component to functional component

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,33 +1,26 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { Col, Row } from "react-flexbox-grid";
 import Masonry from 'react-masonry-css';
 
-import experience from "../assets/experience/experience-post";
+import experiences from "../assets/experience/experience-post";
 import Arrow from "../assets/images/arrow.svg";
 import { ThemeContext } from "./theme-provider";
 import { ExperienceComp } from "./ui-components";
 import { useMediaQuery } from "./ui-components/useMediaQuery"
 
-class Experience extends React.Component {
-	constructor(exp) {
-		super(exp);
-		this.state = {
-			experiencePosts: experience,
-			filterResult: null, 
-			pickedFilter: "all", 
-			filterMenuActive: false, 
-			pickedFilterDropdown: "NEWEST"
-		}
-	}
-	
 
-	componentDidMount() {
-		this.filterGallery("all");
-	}
+export default function Experience () {
+	// const [experience, setExperience] = useState({ experiencePosts: experiences });
+	const [filterResult, setFilterResult] = useState(null); 
+	const [pickedFilter, setPickedFilter] = useState("all"); 
+	const [filterMenuActive, setFilterMenuActive] = useState(false); 
+	const [pickedFilterDropdown, setPickedFilterDropdown] = useState("NEWEST");
+
+	useEffect(() => filterGallery("all"));
 
 	// Filtering Experience by Tag
-	filterGallery = (target) => {
-		let experienceArr = [...this.state.experiencePosts];
+	const filterGallery = (target) => {
+		let experienceArr = [...experiences];
 		let result;
 
 		if (target !== "all") {
@@ -35,28 +28,24 @@ class Experience extends React.Component {
 		} else {
 			result = experienceArr
 		}
-
-		this.setState({
-			filterResult: result,
-			pickedFilter: target,
-			pickedFilterDropdown: "NEWEST",
-		});
+		setFilterResult({ filterResult: result });
+		setPickedFilter({ pickedFilter: target });
+		setPickedFilterDropdown({ pickedFilterDropdown: "NEWEST" });
 	};
 
 	// Filter Drop Down Hover Menu
-	filterMenuHover = (event) => {
-		if (event) {
-			this.setState({ filterMenuActive: true });
-		} else {
-			this.setState({ filterMenuActive: false });
-		}
+	const filterMenuHover = (event) => {
+		(event) 
+		? setFilterMenuActive({ filterMenuActive: true }) 
+		: setFilterMenuActive({ filterMenuActive: false });
 	};
 
 	// Filter drop down handler
-	filterDropDownHandler = (filter) => {
-		this.setState({ pickedFilterDropdown: filter, filterMenuActive: false });
+	const filterDropDownHandler = (filter) => {
+		setPickedFilterDropdown({ pickedFilterDropdown: filter});
+		setFilterMenuActive({ filterMenuActive: false });
 
-		let experienceArr = [...this.state.filterResult];
+		let experienceArr = [...filterResult];
 		let result;
 
 		if (filter === "NEWEST") {
@@ -65,18 +54,13 @@ class Experience extends React.Component {
 			result = experienceArr.sort((a, b) => (a.id > b.id ? 1 : -1)).reverse();
 		}
 
-		this.setState({ filterResult: result });
+		setFilterResult({ filterResult: result });
 	}
 
-	useContextTheme (style) {
-		useContext(style);
-	}
-
-	render() { 
 		// Rendering experience
 		let experienceRender = null;
-		if (this.state.filterResult) {
-			experienceRender = this.state.filterResult.map((experience, index) => (
+		if (experiences) {
+			experienceRender = experiences.map((experience, index) => (
 				<ExperienceComp 
 					title={experience.title} 
 					company={experience.company} 
@@ -94,8 +78,6 @@ class Experience extends React.Component {
 			992: 2,
 			686: 1
 		};
-
-		// const styling = useContext(ThemeContext);
 
 		const expStyle = {
 		// maxWidth: "1200px",
@@ -127,7 +109,7 @@ class Experience extends React.Component {
 	]
 
 		let filterDropDown = null;
-		if (this.state.filterMenuActive) {
+		if (filterMenuActive) {
 			filterDropDown = (
 				<div className="portfolio__filter-menu shadow">
 				<p
@@ -156,7 +138,8 @@ class Experience extends React.Component {
 								{tags.map((post, index) => {
 									return(
 									<li 
-									className={`${this.state.pickedFilter} === ${post.expTag}
+									key={index}
+									className={`${pickedFilter} === ${post.expTag}
 										? return "portfolio__nav-active font12"
 										: return "font12"
 									`} 
@@ -169,11 +152,11 @@ class Experience extends React.Component {
 						<Col xs={12} sm={12} md={4} lg={3}>
 						<div
 							className="portfolio__filter"
-							onMouseEnter={() => this.filterMenuHover(true)}
-							onMouseLeave={() => this.filterMenuHover(false)}
+							onMouseEnter={() => filterMenuHover(true)}
+							onMouseLeave={() => filterMenuHover(false)}
 						>
 							<p className="font12">
-							{this.state.pickedFilterDropdown} FIRST
+							{pickedFilterDropdown} FIRST
 							</p>
 							<i class="fas fa-angle-down"></i>
 							{filterDropDown}
@@ -191,10 +174,192 @@ class Experience extends React.Component {
 			</Masonry>
 		</div>
 		)
-	}
 }
 
-export default Experience;
+// export default Experience;
+
+// class Experience extends React.Component {
+// 	constructor(exp) {
+// 		super(exp);
+// 		this.state = {
+// 			experiencePosts: experience,
+// 			filterResult: null, 
+// 			pickedFilter: "all", 
+// 			filterMenuActive: false, 
+// 			pickedFilterDropdown: "NEWEST"
+// 		}
+// 	}
+	
+
+// 	componentDidMount() {
+// 		this.filterGallery("all");
+// 	}
+
+// 	// Filtering Experience by Tag
+// 	filterGallery = (target) => {
+// 		let experienceArr = [...this.state.experiencePosts];
+// 		let result;
+
+// 		if (target !== "all") {
+// 			result = experienceArr.filter((expTag) => expTag.tag === target)
+// 		} else {
+// 			result = experienceArr
+// 		}
+
+// 		this.setState({
+// 			filterResult: result,
+// 			pickedFilter: target,
+// 			pickedFilterDropdown: "NEWEST",
+// 		});
+// 	};
+
+// 	// Filter Drop Down Hover Menu
+// 	filterMenuHover = (event) => {
+// 		if (event) {
+// 			this.setState({ filterMenuActive: true });
+// 		} else {
+// 			this.setState({ filterMenuActive: false });
+// 		}
+// 	};
+
+// 	// Filter drop down handler
+// 	filterDropDownHandler = (filter) => {
+// 		this.setState({ pickedFilterDropdown: filter, filterMenuActive: false });
+
+// 		let experienceArr = [...this.state.filterResult];
+// 		let result;
+
+// 		if (filter === "NEWEST") {
+// 			result = experienceArr.sort((a, b) => (a.id > b.id ? 1 : -1));
+// 		} else if (filter === "OLDEST") {
+// 			result = experienceArr.sort((a, b) => (a.id > b.id ? 1 : -1)).reverse();
+// 		}
+
+// 		this.setState({ filterResult: result });
+// 	}
+
+// 	render() { 
+// 		// Rendering experience
+// 		let experienceRender = null;
+// 		if (this.state.filterResult) {
+// 			experienceRender = this.state.filterResult.map((experience, index) => (
+// 				<ExperienceComp 
+// 					title={experience.title} 
+// 					company={experience.company} 
+// 					description={experience.description} 
+// 					date={experience.date} 
+// 					location={experience.location}
+// 					key={index}
+// 				/>
+// 			));
+// 		}
+
+// 		// Experience Breakpoints
+// 		const experienceBreakpoints = {
+// 			default: 3,
+// 			992: 2,
+// 			686: 1
+// 		};
+
+// 		const expStyle = {
+// 		// maxWidth: "1200px",
+// 		// background: styling.theme.primary,
+// 		width: "100%",
+// 		height: "100%",
+// 		margin: "0 auto",
+// 		padding: "0 15px"
+// 	}
+
+// 	const tags = [
+// 		{
+// 			expTag: "all",
+// 			text: "ALL"
+// 		},
+// 		{
+// 			expTag: "design",
+// 			text: "DESIGN"
+// 		},
+// 		{
+// 			expTag: "coding",
+// 			text: "Coding"
+// 		},
+// 		{
+// 			expTag: "people interaction",
+// 			text: "People Interaction"
+// 		},
+
+// 	]
+
+// 		let filterDropDown = null;
+// 		if (this.state.filterMenuActive) {
+// 			filterDropDown = (
+// 				<div className="portfolio__filter-menu shadow">
+// 				<p
+// 				  className="font12"
+// 				  onClick={() => this.filterDropDownHandler("NEWEST")}
+// 				>
+// 				  NEWEST
+// 				</p>
+// 				<p
+// 				  className="font12"
+// 				  onClick={() => this.filterDropDownHandler("OLDEST")}
+// 				>
+// 				  OLDEST
+// 				</p>
+// 			  </div>
+// 			);
+// 		}
+
+// 		return (
+// 			<div style={expStyle}>
+// 				<div className="expWrapper">
+// 					<Row>
+// 						<Col xs={12} sm={12} md={8} lg={9}>
+// 						<div className="portfolio__nav">
+// 							<ul>
+// 								{tags.map((post, index) => {
+// 									return(
+// 									<li 
+// 									key={index}
+// 									className={`${this.state.pickedFilter} === ${post.expTag}
+// 										? return "portfolio__nav-active font12"
+// 										: return "font12"
+// 									`} 
+// 									onClick={() => this.filterGallery(`${post.expTag}`)}
+// 									>{post.text}</li>
+// 								)})}
+// 							</ul>
+// 						</div>
+// 						</Col>
+// 						<Col xs={12} sm={12} md={4} lg={3}>
+// 						<div
+// 							className="portfolio__filter"
+// 							onMouseEnter={() => this.filterMenuHover(true)}
+// 							onMouseLeave={() => this.filterMenuHover(false)}
+// 						>
+// 							<p className="font12">
+// 							{this.state.pickedFilterDropdown} FIRST
+// 							</p>
+// 							<i class="fas fa-angle-down"></i>
+// 							{filterDropDown}
+// 						</div>
+// 						</Col>
+// 				</Row>
+// 			</div>
+
+// 			<Masonry 
+// 				breakpointCols={experienceBreakpoints} 
+// 				className="my-masonry-grid"
+// 				columnClassName="my-masonry-grid_column"
+// 			>
+// 				{experienceRender}
+// 			</Masonry>
+// 		</div>
+// 		)
+// 	}
+// }
+
+// export default Experience;
 
 // export default function Experiences () {
 // 	const state = useContext(ThemeContext);
