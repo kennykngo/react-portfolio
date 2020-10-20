c961ac33892fa81d9878d821a01623c40e6e43e6
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-10-19 22:26:06 -0700

    added educationStyle to education-skills to provide dark mode

--- src/components/education-skills.js
+++ src/components/education-skills.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from './theme-provider'
 import {Title} from "./ui-components"
 
 export default function EducationSkills () {
 
+	const state = useContext(ThemeContext);
+	const educationStyle = { 
+		backgroundColor: state.theme.primary,
+	}
+
 	return (
-		<div id="education-skills">
+		<div id="education-skills" style={educationStyle}>
 			<Title>Education and Skills</Title>
+
 		</div>
 	)
 }
