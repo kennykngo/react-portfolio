1361c3a1c5bef7b8162a2abb6a7956f3a6c7cb39
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-10-19 23:20:40 -0700

    fixed issue for education responsive sizing

--- src/components/education-skills.js
+++ src/components/education-skills.js
@@ -13,6 +13,11 @@ export default function EducationSkills () {
 		color: state.theme.text,
 	}
 
+	const pStyle = {
+		color: state.theme.text,
+		fontWeight: state.theme.pFontWeight,
+	}
+
 	const educationPosts = [
 		{ 
 			text: "Visual Communication Design",
@@ -24,6 +29,14 @@ export default function EducationSkills () {
 			location: "University of California, Berkeley, CA",
 			description: "A 24-week intensive program focused on gaining technical programming skills in HTML5, CSS3, Javascript, JQuery, Bootstrap, Firebase, Node Js, MySQL, MongoDB, Express, Handelbars.js & ReactJS."
 		},
+		{ 
+			text: "Language & Application Skills",
+			skills: "",
+			location: "",
+			description: `HTML5, CSS3, JavaScript, ES6, NodeJS, MySQL, MongoDB, jQuery, Express, & ReactJS
+
+			Adobe XD, Sketch, Photoshop, Illustrator, InDesign, PremierePro, Figma`
+		},
 	]
 
 	return (
@@ -32,10 +45,10 @@ export default function EducationSkills () {
 			<div className="education-grid container">
 				{educationPosts.map((posts) => {
 					return (
-					<div  className="post-container">
+					<div className={`post-container ${posts.skills && "skills"}`} >
 						<h2 style={textStyle}>{posts.text}</h2>
 						<h4 style={textStyle}>{posts.location}</h4>
-						<p style={textStyle}>{posts.description}</p>
+						<p style={pStyle}>{posts.description}</p>
 					</div>
 					)
 				})}
