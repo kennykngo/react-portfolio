eb1eb1288265a4e76286c34d344cb6af07204ffe
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-10-29 18:01:33 -0700

    edited the errors (experience dates and deployment links)

--- src/assets/posts/experience-post.js
+++ src/assets/posts/experience-post.js
@@ -1,56 +1,62 @@
 export default [
-	{
-		id: "1",
-		title: "Web and Graphic Designer",
-		company: "San Francisco State University",
-		tag: "design",
-		description: "Generated graphics to increase survey participation by over 200%. Produced flyers, websites using Drupal, and a logo to be viewed by over 35,000 faculties and students campus-wide.",
-		date: "Aug 2019 - Jun 2020",
-		location: "San Francisco, CA"
-	}, 
-	{
-		id: "2",
-		title: "Graphic Design Intern (Unpaid)",
-		company: "ViviendasLeón",
-		tag: "design",
-		description: "Worked with ViviendasLeón’s Global Director to organize dozens of RAW images and footages into a finalized product to increase social media by 250%. Designed videos and infographics about the non-profit organization’s work to be placed on their website",
-		date: "Jan 2020 - Jun 2020",
-		location: "San Francisco, CA"
-	}, 
-	{
-		id: "3",
-		title: "3602 Library Page",
-		company: "San Francisco Public Library",
-		tag: "people interaction",
-		description: "Generated graphics to increase survey participation by over 200%. Produced flyers, websites using Drupal, and a logo to be viewed by over 35,000 faculties and students campus-wide.",
-		date: "Apr 2018 - Dec 2018",
-		location: "San Francisco, CA"
-	}, 
-	{
-		id: "4",
-		title: "Barista",
-		company: "Starbucks",
-		tag: "people interaction",
-		description: "Generated graphics to increase survey participation by over 200%. Produced flyers, websites using Drupal, and a logo to be viewed by over 35,000 faculties and students campus-wide.",
-		date: "Aug 2015 - Sep 2028",
-		location: "San Francisco, CA"
-	}, 
-	{
-		id: "5",
-		title: "Explainer",
-		company: "Exploratorium",
-		tag: "people interaction",
-		description: "Elicidated exhibits and guided 100+ people everyday around the museum",
-		date: "Oct 2013 - Feb 2014",
-		location: "San Francisco, CA"
-	}, 
-	{
-		id: "6",
-		title: "Public Relations Intern",
-		company: "Bay Area Air Quality Management District",
-		tag: "people interaction",
-		description: "Explained the importance of air quality to 120+ individuals, from CEOs to girl scouts.",
-		date: "Jun 2012 - Aug 2012",
-		location: "San Francisco, CA"
-	}, 
-]
+  {
+    id: "1",
+    title: "Web and Graphic Designer",
+    company: "San Francisco State University",
+    tag: "design",
+    description:
+      "Generated graphics to increase survey participation by over 200%. Produced flyers, websites using Drupal, and a logo to be viewed by over 35,000 faculties and students campus-wide.",
+    date: "Aug 2019 - Jun 2020",
+    location: "San Francisco, CA",
+  },
+  {
+    id: "2",
+    title: "Graphic Design Intern (Unpaid)",
+    company: "ViviendasLeón",
+    tag: "design",
+    description:
+      "Worked with ViviendasLeón’s Global Director to organize dozens of RAW images and footages into a finalized product to increase social media by 250%. Designed videos and infographics about the non-profit organization’s work to be placed on their website",
+    date: "Jan 2020 - Jun 2020",
+    location: "San Francisco, CA",
+  },
+  {
+    id: "3",
+    title: "3602 Library Page",
+    company: "San Francisco Public Library",
+    tag: "people interaction",
+    description:
+      "Generated graphics to increase survey participation by over 200%. Produced flyers, websites using Drupal, and a logo to be viewed by over 35,000 faculties and students campus-wide.",
+    date: "Apr 2018 - Dec 2018",
+    location: "San Francisco, CA",
+  },
+  {
+    id: "4",
+    title: "Barista",
+    company: "Starbucks",
+    tag: "people interaction",
+    description:
+      "Generated graphics to increase survey participation by over 200%. Produced flyers, websites using Drupal, and a logo to be viewed by over 35,000 faculties and students campus-wide.",
+    date: "Aug 2015 - Sep 2018",
+    location: "San Francisco, CA",
+  },
+  {
+    id: "5",
+    title: "Explainer",
+    company: "Exploratorium",
+    tag: "people interaction",
+    description:
+      "Elicidated exhibits and guided 100+ people everyday around the museum",
+    date: "Oct 2013 - Feb 2014",
+    location: "San Francisco, CA",
+  },
+  {
+    id: "6",
+    title: "Public Relations Intern",
+    company: "Bay Area Air Quality Management District",
+    tag: "people interaction",
+    description:
+      "Explained the importance of air quality to 120+ individuals, from CEOs to girl scouts.",
+    date: "Jun 2012 - Aug 2012",
+    location: "San Francisco, CA",
+  },
+];
--- src/assets/posts/projects-post.js
+++ src/assets/posts/projects-post.js
@@ -1,29 +1,32 @@
-export default [ 
-	{
-		title: "Mumble",
-		description: "The Dating App for Introverts",
-		point: "A mobile-first designed app that pairs individuals based on a complex algorithm.",
-		githubLink: "https://github.com/kennykngo/dating-app",
-		deployLink: "https://mumble-dating-app.herokuapp.com/",
-		image: "mumble-app.png",
-		categories: ["React", "MongoDB", "JavaScript"],
-	},
-	{
-		title: "Pokemon Trade",
-		description: "Trade Pokemon Again",
-		point: "An app that allows you to reminisce as you keep track of your Pokemon, chat with others to trade, and even look up various information on your little critters. ",
-		githubLink: "https://github.com/kennykngo/Pokedex-API",
-		deployLink: "https://pokemon-trade-heroku.herokuapp.com/",
-		image: "pokemon-trade.png",
-		categories: ["MongoDB", "JavaScript"],
-	},
-	{
-		title: "Weather Dashboard",
-		description: "Track the weather.",
-		point: "Displays weather, AQI, wind-speed, etc. of the city you select.",
-		githubLink: "https://github.com/kennykngo/dating-app",
-		deployLink: "https://mumble-dating-app.herokuapp.com/",
-		image: "weather-dashboard.png",
-		categories: ["HTML", "CSS", "JavaScript"],
-	}
-]
+export default [
+  {
+    title: "Mumble",
+    description: "The Dating App for Introverts",
+    point:
+      "A mobile-first designed app that pairs individuals based on a complex algorithm.",
+    githubLink: "https://github.com/kennykngo/dating-app",
+    deployLink: "https://mumble-dating-app.herokuapp.com/",
+    image: "mumble-app.png",
+    categories: ["React", "MongoDB", "JavaScript"],
+  },
+  {
+    title: "Pokemon Trade",
+    description: "Trade Pokemon Again",
+    point:
+      "An app that allows you to reminisce as you keep track of your Pokemon, chat with others to trade, and even look up various information on your little critters. ",
+    githubLink: "https://github.com/kennykngo/Pokedex-API",
+    deployLink: "https://pokemon-trade-heroku.herokuapp.com/",
+    image: "pokemon-trade.png",
+    categories: ["MongoDB", "JavaScript"],
+  },
+  {
+    title: "Weather Dashboard",
+    description: "Track the weather.",
+    point:
+      "Displays weather, AQI, wind-speed, etc. of the city you select. Learning to utilize APIs.",
+    githubLink: "https://github.com/kennykngo/bcs_weatherApp",
+    deployLink: "https://kennykngo.github.io/bcs_weatherApp/",
+    image: "weather-dashboard.png",
+    categories: ["HTML", "CSS", "JavaScript"],
+  },
+];
