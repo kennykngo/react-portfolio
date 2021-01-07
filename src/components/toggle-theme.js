e269326adb35df3659b7654aa17d7f5ae3c89b5f
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-07 13:21:34 -0800

    added another toggle-theme.scss and removed normal text from label

--- src/components/toggle-theme.js
+++ src/components/toggle-theme.js
@@ -2,51 +2,42 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import FormGroup from "@material-ui/core/FormGroup";
 import Switch from "@material-ui/core/Switch";
 import React, { useContext } from "react";
-import styled from "styled-components";
 
-// import Button from "./button-ripple";
 import { ThemeContext } from "./theme-provider";
+
+// import styled from "styled-components";
+
+import "./__toggle-theme.scss";
 
 export default function ToggleTheme() {
   const state = useContext(ThemeContext);
   const [checked, setChecked] = React.useState(false);
 
-  const Button = styled.button`
-    text-decoration: none;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-    transition: background 400ms;
-    background-color: ${state.theme.primary};
-    outline: 0;
-    border-radius: 0.25rem;
+  // const Button = styled.button`
+  //   text-decoration: none;
+  //   cursor: pointer;
+  //   position: relative;
+  //   overflow: hidden;
+  //   transition: background 400ms;
+  //   background-color: ${state.theme.primary};
+  //   outline: 0;
+  //   border-radius: 0.25rem;
 
-    &:active {
-      text-decoration: none;
-    }
+  //   &:active {
+  //     text-decoration: none;
+  //   }
 
-    &:hover {
-      background-color: ${state.theme.hoverBtn};
-      color: #f8f9fa;
-    }
-  `;
+  //   &:hover {
+  //     background-color: ${state.theme.hoverBtn};
+  //     color: #f8f9fa;
+  //   }
+  // `;
 
   const toggleChecked = () => {
     setChecked((prev) => !prev);
   };
 
   return (
-    // <Button
-    //   className="mb-5"
-    //   variant="contained"
-    //   onClick={() => {
-    //     state.setTheme(state.theme.type);
-    //   }}
-    //   // style={btn}
-    // >
-    //   Toggle Theme
-    // </Button>
-
     <FormGroup>
       <FormControlLabel
         control={
@@ -58,7 +49,6 @@ export default function ToggleTheme() {
             onChange={toggleChecked}
           />
         }
-        label="Normal"
       />
     </FormGroup>
   );
