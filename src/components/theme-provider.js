c9a10936a755f1e9645904fb220e5a2f2ce6a046
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-07 13:47:35 -0800

    added styles to ExperienceComp's p tags

--- src/components/theme-provider.js
+++ src/components/theme-provider.js
@@ -1,57 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export const ThemeContext = React.createContext({
   theme: {
-    type: 'light',
-    primary: '#f8f9fa',
-    text: '#black'
+    type: "light",
+    primary: "#f8f9fa",
+    text: "#black",
   },
-  setTheme: () => {}
-})
+  setTheme: () => {},
+});
 
-export const ThemeContextProvider = props => {
+export const ThemeContextProvider = (props) => {
   const theme = {
     light: {
-      type: 'light',
-      primary: '#f8f9fa',
-      primaryBtn: '#f8f9fa',
-      text: '#444444',
+      type: "light",
+      primary: "#f8f9fa",
+      primaryBtn: "#f8f9fa",
+      text: "#444444",
       primaryNav: "#ffffff",
       invert: "invert(0.25)",
       pFontWeight: "1",
       hover: "#f8f9fa",
       card: "#ffffff",
       hoverBtn: "#1890ff",
-
     },
-    dark:{
-      type: 'dark',
-      primary: '#333333',
-      primaryBtn: '#333333',
-      text: '#ccc',
+    dark: {
+      type: "dark",
+      primary: "#333333",
+      primaryBtn: "#333333",
+      text: "#ccc",
       primaryNav: "#121212",
       invert: "invert(0.75)",
-      pFontWeight: "51",
+      pFontWeight: "1",
       hover: "#f8f9fa",
       card: "#212121",
       hoverBtn: "#1890ff",
     },
-  }
+  };
 
-  const setTheme = type => {
-    setState({ ...state, theme: type === 'dark' ? theme.light : theme.dark })
-  }
+  const setTheme = (type) => {
+    setState({ ...state, theme: type === "dark" ? theme.light : theme.dark });
+  };
 
   const initState = {
     theme: theme.light,
-    setTheme: setTheme
-  }
+    setTheme: setTheme,
+  };
 
-  const [state, setState] = useState(initState)
+  const [state, setState] = useState(initState);
 
   return (
     <ThemeContext.Provider value={state}>
       {props.children}
     </ThemeContext.Provider>
-  )
-}
+  );
+};
--- src/components/ui-components/experience-comp.js
+++ src/components/ui-components/experience-comp.js
@@ -1,33 +1,33 @@
-import React, {useContext} from 'react';
-import {ThemeContext} from "../theme-provider";
+import React, { useContext } from "react";
+import { ThemeContext } from "../theme-provider";
 
-export default function ExperienceComp (props) { 
+export default function ExperienceComp(props) {
+  const state = useContext(ThemeContext);
 
-	const state = useContext(ThemeContext);
+  const headingStyle = {
+    backgroundColor: "transparent",
+    color: state.theme.text,
+    fontWeight: 800,
+  };
 
-	const headingStyle = {
-			backgroundColor: "transparent",
-			color: state.theme.text,
-			fontWeight: 800,
-	}
+  const pStyle = {
+    color: state.theme.text,
+    backgroundColor: "transparent",
+    fontWeight: state.theme.pFontWeight,
+  };
 
-	const pStyle = {
-		color: state.theme.text,
-		backgroundColor: "transparent",
-	}
+  const dateStyle = {
+    color: "#2997ff",
+  };
 
-	const dateStyle = {
-		color: "#2997ff"
-	}
-
-	return(
-	<div className="experience-box">
-		<h2 style={headingStyle}>{props.title}</h2>
-		<h4 style={pStyle}>{props.company}</h4>
-		<p style={pStyle}>{props.location}</p>
-		<p style={pStyle}>{props.tag}</p>
-		<p style={pStyle}>{props.description}</p>
-		<p style={dateStyle}>{props.date}</p>
-	</div>	
-	)
+  return (
+    <div className="experience-box">
+      <h2 style={headingStyle}>{props.title}</h2>
+      <h4 style={pStyle}>{props.company}</h4>
+      <p style={pStyle}>{props.location}</p>
+      <p style={pStyle}>{props.tag}</p>
+      <p style={pStyle}>{props.description}</p>
+      <p style={dateStyle}>{props.date}</p>
+    </div>
+  );
 }
